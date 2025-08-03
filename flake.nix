83aac32779645e07c5eb3b44db0ef5f06f8967a1
Morgan Jones <me@numin.it>
2025-08-03 01:42:01 +0000

    gvh: Converge on a final job cores/memory

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
           JOB_ATTR="$(${hostPkgs.lib.getExe hostPkgs.jq} -r '.attr' <<< "$JSON_LINE")"
           JOB_DRV="$(realpath -- "$(${hostPkgs.lib.getExe hostPkgs.jq} -r '.drvPath' <<< "$JSON_LINE")")"
 
-          exec nix-build -E \
+          nix-build -E \
             "(import $JOB_DRV).all" \
             --out-link "$JOB_ATTR" \
             --max-jobs 0 \
@@ -29,69 +29,114 @@
 
         cacheDownloadScriptFor =
           system: nixpkgs:
-          hostPkgs.writeShellScript "cache-pkgs" ''
-            if [ -z "$EVAL_WORKERS" ]; then
-             EVAL_WORKERS="$(nproc)"
-             EVAL_WORKERS=$((EVAL_WORKERS/4))
-            fi
-            if [ "$EVAL_WORKERS" -lt 8 ]; then
-              EVAL_WORKERS=8
-            fi
-            if [ -z "$DL_WORKERS" ]; then
-             DL_WORKERS="$(nproc)"
-             DL_WORKERS=$((DL_WORKERS/4))
-            fi
-            if [ "$DL_WORKERS" -lt 1 ]; then
-              DL_WORKERS=1
-            fi
-            if [ -z "$MEM_PER_EVAL_WORKER" ]; then
-              MEM_PER_EVAL_WORKER=2048
-            fi
-            if [[ -z "$CACHE_GCROOTS_DIR" ]]; then
-              CACHE_GCROOTS_DIR="$(pwd)"
-            fi
-            SHORT_NIX_VSN="${hostPkgs.lib.version}"
+          hostPkgs.writeShellApplication {
+            name = "great-value-hydra";
+            text = ''
+              set -euo pipefail
 
-            echo "Great Value Hydra: it's just someone else's Hydra" >&2
-            echo "Using $EVAL_WORKERS eval workers, $DL_WORKERS download workers" >&2
+              if [[ ! -v MIN_MEM_PER_WORKER ]]; then
+                MIN_MEM_PER_WORKER=2048
+              fi
 
-            set -euo pipefail
+              if [[ ! -v HEADROOM ]]; then
+                HEADROOM=2048
+              fi
+              if [[ ! -v DL_WORKERS ]]; then
+                DL_WORKERS=0
+              fi
+              if [[ ! -v CACHE_GCROOTS_DIR ]]; then
+                CACHE_GCROOTS_DIR="$(pwd)"
+              fi
 
-            ROOT_DIR="$CACHE_GCROOTS_DIR/$SHORT_NIX_VSN/${system}"
-            echo "Caching to $ROOT_DIR" >&2
-            BUILT_GCROOTS_DIR="$ROOT_DIR/built"
-            DRV_GCROOTS_DIR="$ROOT_DIR/drvs"
-            DRVINFO="$ROOT_DIR/drvinfo.json"
+              echo "Great Value Hydra: it's just someone else's Hydra" >&2
 
-            mkdir -p "$BUILT_GCROOTS_DIR" "$DRV_GCROOTS_DIR"
+              echo "Checking parallel" >&2
+              if ! parallel --citation &>/dev/null </dev/null; then
+                parallel --citation || exit $?
+              fi
 
-            if [ ! -f "$DRVINFO" ]; then
-              echo "Evaluating package set. This will take a long time and use a lot of memory." >&2
+              echo "Determining optimal eval headroom" >&2
 
-              ${hostPkgs.nix-eval-jobs}/bin/nix-eval-jobs \
-                --force-recurse \
-                --impure \
-                --gc-roots-dir "$DRV_GCROOTS_DIR" \
-                --max-memory-size "$MEM_PER_EVAL_WORKER" \
-                --workers "$EVAL_WORKERS" ${nixpkgs}/pkgs/top-level/release.nix > "$DRVINFO"
+              # Determine the number of eval workers iteratively.
+              num_workers="$(nproc)"
+              mem_per_worker=0
+              while [ "$num_workers" -gt 1 ]; do
+                # Compute the memory to use for eval.
+                total_mem="$(awk '/MemFree/ { printf "%d\n", int($2/1024) }' /proc/meminfo)"
+                total_mem=$((total_mem - HEADROOM))
 
-              cat "$DRVINFO" | ${hostPkgs.lib.getExe hostPkgs.jq} -rc 'select(.error == null)' | \
-                ${hostPkgs.moreutils}/bin/sponge "$DRVINFO" 2>/dev/null || true
-            fi
+                # Compute the usable memory per worker.
+                mem_per_worker=$((total_mem / num_workers))
+                if [ "$mem_per_worker" -lt "$MIN_MEM_PER_WORKER" ]; then
+                  # Converge
+                  echo "rejected: $total_mem mem, $num_workers workers, $mem_per_worker mem per worker" >&2
+                  num_workers=$(((num_workers * 7) / 8))
+                else
+                  break
+                fi
+              done
 
-            echo "About to cache $(cat "$DRVINFO" | wc -l) packages. Godspeed." >&2
+              if [ "$DL_WORKERS" -lt 1 ]; then
+                DL_WORKERS="$num_workers"
+              fi
 
-            pushd .
-            cd "$BUILT_GCROOTS_DIR"
+              echo "Using $num_workers eval workers at $mem_per_worker MiB/worker ($((num_workers * mem_per_worker)) MiB, $HEADROOM MiB headroom)." >&2
+              echo "Using $DL_WORKERS download workers." >&2
 
-            <"$DRVINFO" \
-            nice -n 20 ${hostPkgs.parallel}/bin/parallel \
-              --bar --eta \
-              -j"$DL_WORKERS" \
-              ${runNixJob} '{}'
+              nixpkgs_ver="${hostPkgs.lib.version}"
+              root_dir="$CACHE_GCROOTS_DIR/$nixpkgs_ver/${system}"
+              echo "Caching to $root_dir" >&2
 
-            popd
-          '';
+              built_gcroots_dir="$root_dir/built"
+              drv_gcroots_dir="$root_dir/drvs"
+              drvinfo="$root_dir/drvinfo.json"
+
+              mkdir -p "$root_dir" "$built_gcroots_dir" "$drv_gcroots_dir"
+
+              if [ ! -f "$drvinfo" ]; then
+                echo "Evaluating package set. This will take a long time and use a lot of memory." >&2
+                echo "Note that processes dying here will probably cause a lot of problems." >&2
+                nix_eval_jobs_log="$root_dir/nix-eval-jobs.log"
+
+                nix-eval-jobs \
+                  --force-recurse \
+                  --impure \
+                  --gc-roots-dir "$drv_gcroots_dir" \
+                  --max-memory-size "$mem_per_worker" \
+                  --workers "$num_workers" ${nixpkgs}/pkgs/top-level/release.nix > "$drvinfo" 2> >(tee -a "$nix_eval_jobs_log" >&2)
+              fi
+
+              filtered_drvinfo="$(mktemp -t great-value-hydra.XXXXXX)"
+              jq -rc 'select(.error == null)' < "$drvinfo" | \
+                sponge "$filtered_drvinfo" 2>/dev/null || true
+
+              echo "Filtering drvinfo at $drvinfo ($(wc -l < "$filtered_drvinfo") packages)" >&2
+              cleanup() {
+                rm -f "$filtered_drvinfo"
+              }
+              trap cleanup EXIT
+
+              echo "About to cache $(wc -l < "$filtered_drvinfo") packages. Godspeed." >&2
+
+              pushd .
+              cd "$built_gcroots_dir"
+
+              nice -n 20 parallel \
+                --bar --eta \
+                -j"$DL_WORKERS" \
+                ${runNixJob} '{}' <"$filtered_drvinfo"
+
+              popd
+            '';
+            runtimeInputs = with hostPkgs; [
+              jq
+              parallel
+              # has its own version of parallel, put it after
+              moreutils
+              gawk
+              nix-eval-jobs
+            ];
+          };
 
       in
       {
@@ -111,7 +156,7 @@
         apps =
           let
             mkApp = program: {
-              program = "${self.packages.${system}.${program}}";
+              program = "${hostPkgs.lib.getExe self.packages.${system}.${program}}";
               type = "app";
             };
           in

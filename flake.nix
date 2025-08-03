4d2b103c48d4f1f6f975c570b7d327121ad6ca82
Morgan Jones <me@numin.it>
2025-08-03 00:34:17 +0000

    Memoize eval results

--- flake.nix
+++ flake.nix
@@ -52,42 +52,45 @@
             fi
             SHORT_NIX_VSN="${hostPkgs.lib.version}"
 
+            echo "Great Value Hydra: it's just someone else's Hydra" >&2
             echo "Using $EVAL_WORKERS eval workers, $DL_WORKERS download workers" >&2
 
             set -euo pipefail
 
-            echo "Caching to $CACHE_GCROOTS_DIR/$SHORT_NIX_VSN/${system}/..." >&2
-            BUILT_GCROOTS_DIR="$CACHE_GCROOTS_DIR/$SHORT_NIX_VSN/${system}/built"
-            DRV_GCROOTS_DIR="$CACHE_GCROOTS_DIR/$SHORT_NIX_VSN/${system}/drvs"
+            ROOT_DIR="$CACHE_GCROOTS_DIR/$SHORT_NIX_VSN/${system}"
+            echo "Caching to $ROOT_DIR" >&2
+            BUILT_GCROOTS_DIR="$ROOT_DIR/built"
+            DRV_GCROOTS_DIR="$ROOT_DIR/drvs"
+            DRVINFO="$ROOT_DIR/drvinfo.json"
 
             mkdir -p "$BUILT_GCROOTS_DIR" "$DRV_GCROOTS_DIR"
 
-            DRVINFO="$(mktemp)"
-            echo "Evaluating package set" >&2
+            if [ ! -f "$DRVINFO" ]; then
+              echo "Evaluating package set. This will take a long time and use a lot of memory." >&2
 
-            ${hostPkgs.nix-eval-jobs}/bin/nix-eval-jobs \
-              --force-recurse \
-              --impure \
-              --gc-roots-dir "$DRV_GCROOTS_DIR" \
-              --max-memory-size "$MEM_PER_EVAL_WORKER" \
-              --workers "$EVAL_WORKERS" ${nixpkgs}/pkgs/top-level/release.nix > "$DRVINFO"
+              ${hostPkgs.nix-eval-jobs}/bin/nix-eval-jobs \
+                --force-recurse \
+                --impure \
+                --gc-roots-dir "$DRV_GCROOTS_DIR" \
+                --max-memory-size "$MEM_PER_EVAL_WORKER" \
+                --workers "$EVAL_WORKERS" ${nixpkgs}/pkgs/top-level/release.nix > "$DRVINFO"
 
-            cat "$DRVINFO" | ${hostPkgs.lib.getExe hostPkgs.jq} -rc 'select(.error == null)' | \
-              ${hostPkgs.moreutils}/bin/sponge "$DRVINFO" 2>/dev/null || true
+              cat "$DRVINFO" | ${hostPkgs.lib.getExe hostPkgs.jq} -rc 'select(.error == null)' | \
+                ${hostPkgs.moreutils}/bin/sponge "$DRVINFO" 2>/dev/null || true
+            fi
 
-            echo "About to cache $(cat "$DRVINFO" | wc -l) packages" >&2
+            echo "About to cache $(cat "$DRVINFO" | wc -l) packages. Godspeed." >&2
 
             pushd .
             cd "$BUILT_GCROOTS_DIR"
 
-            <$DRVINFO \
+            <"$DRVINFO" \
             nice -n 20 ${hostPkgs.parallel}/bin/parallel \
               --bar --eta \
               -j"$DL_WORKERS" \
               ${runNixJob} '{}'
 
             popd
-            rm $DRVINFO
           '';
 
       in
